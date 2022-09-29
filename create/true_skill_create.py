1c6833925873045bf9ef9c3bd7c27f38b69f9d7e
kansei <kansei@kansei.local>
2022-09-29 22:47:17 +0900

    add jockey in true_skill

--- create/true_skill_create.py
+++ create/true_skill_create.py
@@ -6,13 +6,14 @@ import datetime
 import trueskill
 
 def main():
-    result = {}
     horce_rating_data = {}
-    env = trueskill.TrueSkill( draw_probability = 0 )
+    jockey_rating_data = {}
+    env = trueskill.TrueSkill( draw_probability = 0, beta = 12 )
     race_data = dm.pickle_load( "race_data.pickle", prod = True )
     horce_data = dm.pickle_load( "horce_data_storage.pickle", prod = True )
     race_day = dm.pickle_load( "race_day.pickle", prod = True )
-
+    race_jockey_id_data = dm.pickle_load( "race_jockey_id_data.pickle" )
+    
     sort_time_data = []
 
     for k in race_data.keys():
@@ -31,10 +32,15 @@ def main():
         day = race_id[9]
         num = race_id[7]
 
+        try:
+            jockey_id_list = race_jockey_id_data[race_id]
+        except:
+            continue
+
         rank_list = []
         rating_list = []
-        horce_id_list = []
-        lib.dic_append( result, race_id, {} )
+        use_jockey_id_list = []
+        use_horce_id_list = []
 
         for kk in race_data[k].keys():
             horce_id = kk
@@ -47,9 +53,19 @@ def main():
                 continue
 
             try:
-                current_rating = horce_rating_data[horce_id]
+                jockey_id = jockey_id_list[horce_id]
             except:
-                current_rating = env.create_rating()
+                continue
+            
+            try:
+                jockey_current_rating = jockey_rating_data[jockey_id]
+            except:
+                jockey_current_rating = env.create_rating()
+
+            try:
+                horce_current_rating = horce_rating_data[horce_id]
+            except:
+                horce_current_rating = env.create_rating()
 
             rank = cd.rank()
 
@@ -57,18 +73,27 @@ def main():
                 continue
 
             rank_list.append( int( rank - 1 ) )
-            rating_list.append( ( copy.deepcopy( current_rating ), ) )
-            horce_id_list.append( horce_id )
+            use_jockey_id_list.append( jockey_id )
+            use_horce_id_list.append( horce_id )
+            rating_list.append( ( copy.deepcopy( horce_current_rating ), copy.deepcopy( jockey_current_rating ) ) )
 
-        if len( horce_id_list ) < 2:
+        if len( use_horce_id_list ) < 2:
             continue
 
         next_rating_list = env.rate( rating_list, ranks=rank_list )
 
         for i in range( 0, len( next_rating_list ) ):
-            horce_rating_data[horce_id_list[i]] = copy.deepcopy( next_rating_list[i][0] )
-            result[race_id][horce_id_list[i]] = copy.deepcopy( next_rating_list[i][0].mu )
+            horce_rating_data[use_horce_id_list[i]] = copy.deepcopy( next_rating_list[i][0] )
+            jockey_rating_data[use_jockey_id_list[i]] = copy.deepcopy( next_rating_list[i][1] )
 
+    result = { "horce": {}, "jockey": {} }
+
+    for horce_id in horce_rating_data.keys():
+        result["horce"][horce_id] = horce_rating_data[horce_id].mu
+
+    for jockey_id in jockey_rating_data.keys():
+        result["jockey"][jockey_id] = jockey_rating_data[jockey_id].mu
+        
     dm.pickle_upload( "true_skill_data.pickle", result, prod = True )
 
 if __name__ == "__main__":
